f26b2ab4fd81aa6eb64d3a1f1b027917aeda5eb6
Dave Rogers <dave@incnition.com>
2019-10-08 16:29:06 -0700

    add material-ui components for basic styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,33 @@
 import 'cross-fetch/polyfill';
+
 import { GraphQLProvider } from 'graphql-react'
 import { withGraphQLApp } from 'next-graphql-react'
-import NextApp, { Container } from 'next/app'
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import NextApp from 'next/app';
+
+import theme from '../styles/theme';
 
 class App extends NextApp {
-  render() {
-    const { Component, pageProps, graphql } = this.props
-    return (
-      <Container>
-        <GraphQLProvider graphql={graphql}>
-          <Component {...pageProps} />
-        </GraphQLProvider>
-      </Container>
-    )
-  }
+    componentDidMount() {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector("#jss-server-side");
+        if (jssStyles) {
+            jssStyles.parentNode.removeChild(jssStyles);
+        }
+    }
+
+    render() {
+        const { Component, pageProps, graphql } = this.props;
+        return (
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <GraphQLProvider graphql={graphql}>
+                    <Component {...pageProps} />
+                </GraphQLProvider>
+            </ThemeProvider>
+        );
+    }
 }
 
 export default withGraphQLApp(App)
